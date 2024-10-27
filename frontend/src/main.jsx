2cf3f4f4b0f012784d45b52be020a167ea411072
Antoanet Simeonova <antoanet.simeonova@gmail.com>
2024-10-27 03:08:22 +0200

    suggested update for main

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,26 @@
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import App from './App.jsx'
+
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
+
+const root = createRoot(document.getElementById('root'));
+root.render(
   <StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </StrictMode>,
-)
+);
