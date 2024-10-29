f57a10b7fae11ae79859761bcab45b64e8bb4ab9
Antoanet Simeonova <antoanet.simeonova@gmail.com>
2024-10-29 14:24:02 +0200

    added options for user dropdown menu

--- frontend/src/components/UserDropdown.jsx
+++ frontend/src/components/UserDropdown.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useRef, useEffect } from "react";
 import MailOutlineIcon from "@mui/icons-material/MailOutline";
 import LogoutIcon from "@mui/icons-material/Logout";
+import LoginIcon from "@mui/icons-material/Login";
 import PermIdentityRoundedIcon from "@mui/icons-material/PermIdentityRounded";
 import { useNavigate } from "react-router-dom";
 import axiosInstance from "../service/axiosInstance";
@@ -9,9 +10,9 @@ const UserDropdown = () => {
   const [isOpen, setIsOpen] = useState(false);
   const dropdownRef = useRef(null);
   const [error, setError] = useState("");
-
-  // const history = useHistory();
   const navigate = useNavigate();
+  const token = localStorage.getItem("token");
+  const isLoggedIn = Boolean(token);
 
   const toggleDropdown = () => {
     setIsOpen((prev) => !prev);
@@ -33,6 +34,10 @@ const UserDropdown = () => {
     }
   };
 
+  const handleLoginRedirect = () => {
+    navigate("/login");
+  };
+
   const handleClickOutside = (event) => {
     if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
       setIsOpen(false);
@@ -50,23 +55,36 @@ const UserDropdown = () => {
   return (
     <div className="user-dropdown" ref={dropdownRef}>
       <button onClick={toggleDropdown} className="user-button">
-        <PermIdentityRoundedIcon sx={{ fontSize: 38 }} />
+        <PermIdentityRoundedIcon sx={{ fontSize: 30 }} />
         <span className="tooltip-text">User</span>
       </button>
       {isOpen && (
         <div className="dropdown-menu">
-          <button className="dropdown-item" title="Messages">
-            <MailOutlineIcon fontSize="small" />
-            <span>Messages</span>
-          </button>
-          <button
-            className="dropdown-item"
-            onClick={handleLogout}
-            title="Logout"
-          >
-            <LogoutIcon fontSize="small" />
-            <span>Logout</span>
-          </button>
+          {!isLoggedIn ? (
+            <button
+              onClick={handleLoginRedirect}
+              title="Login"
+              className="dropdown-item"
+            >
+              <LoginIcon fontSize="small" />
+              <span>Login</span>
+            </button>
+          ) : (
+            <>
+              <button className="dropdown-item" title="Messages">
+                <MailOutlineIcon fontSize="small" />
+                <span>Messages</span>
+              </button>
+              <button
+                className="dropdown-item"
+                onClick={handleLogout}
+                title="Logout"
+              >
+                <LogoutIcon fontSize="small" />
+                <span>Logout</span>
+              </button>
+            </>
+          )}
         </div>
       )}
     </div>
