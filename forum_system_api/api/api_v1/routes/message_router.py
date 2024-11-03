f20f2d9ead81e525c50c556bb82cc910a1771cfb
Hristiyan <175059014+hbobev@users.noreply.github.com>
2024-11-03 04:28:39 +0200

    feat: add endpoint to retrieve messages for a specific receiver
    - Added GET /messages/{receiver_id} endpoint to retrieve messages for a specific receiver.
    - Implemented get_messages function to fetch messages between the current user and the specified receiver.
    - Returned the list of messages in the conversation or an empty list if no conversation exists.

--- forum_system_api/api/api_v1/routes/message_router.py
+++ forum_system_api/api/api_v1/routes/message_router.py
@@ -1,4 +1,6 @@
-from fastapi import APIRouter, Depends
+from uuid import UUID
+
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 from forum_system_api.persistence.database import get_db
@@ -6,6 +8,7 @@ from forum_system_api.persistence.models.user import User
 from forum_system_api.schemas.message import MessageCreate, MessageResponse
 from forum_system_api.services.message_service import send_message
 from forum_system_api.services.auth_service import get_current_user
+from forum_system_api.services import user_service
 
 
 message_router = APIRouter(prefix="/messages", tags=["messages"])
@@ -23,3 +26,32 @@ def create_message(
     db: Session = Depends(get_db)
 ) -> MessageResponse:
     return send_message(db, message_data, user)
+
+
+@message_router.get(
+        "/{receiver_id}",
+        response_model=list[MessageResponse], 
+        description="Read a message"
+)
+def get_messages(
+    receiver_id: UUID, 
+    user: User = Depends(get_current_user), 
+    db: Session = Depends(get_db)
+) -> list[MessageResponse]:
+    receiver = user_service.get_by_id(user_id=receiver_id, db=db)
+    if receiver is None:
+        raise HTTPException(status_code=401, detail="User not found")
+    
+    conversation = next(
+        (
+            conversation 
+            for conversation in user.conversations 
+            if conversation.user1_id == receiver_id or conversation.user2_id == receiver_id
+        ), 
+        None
+    )
+
+    if conversation is None:
+        return []
+    
+    return conversation.messages
