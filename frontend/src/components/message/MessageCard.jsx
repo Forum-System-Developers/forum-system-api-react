536ea61f3586c4fd60458409240bb7fdd03475c7
Hristiyan <175059014+hbobev@users.noreply.github.com>
2024-11-07 03:50:43 +0200

    feat: add className prop to MessageCard for improved styling flexibility

--- frontend/src/components/message/MessageCard.jsx
+++ frontend/src/components/message/MessageCard.jsx
@@ -1,9 +1,9 @@
 import { Card, CardContent, Typography } from '@mui/material';
 
 
-const MessageCard = ({ message, author, timestamp }) => {
+const MessageCard = ({ className, message, author, timestamp }) => {
     return (
-        <Card sx={{ minWidth: 275 }}>
+        <Card class={className} sx={{ minWidth: 275 }}>
             <CardContent>
                 <Typography gutterBottom sx={{ color: 'text.secondary', fontSize: 14 }}>
                     [{timestamp}] {author}
